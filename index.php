c94f5e5c9b8d2fa4d1c500fbc1da5af4911f077d
BMayank0405 <mayankbarthwal58@gmail.com>
2016-12-14 07:41:01 +0530

    to do list front end

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+   <TITLE>TO DO LIST</TITLE>
+   <meta charset="UTF-8">
+   <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+   <link rel="stylesheet" href="css/main.css" >
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</HEAD>
+<BODY>
+   <div class="list">
+      <h1 class="header" >To do</h1>
+      <ul class="items">
+         <li>
+            <span class="item">first</span><a href="#" class="done-button">Mark as done</a>
+         </li>
+         <li>
+            <span class="item done">second</span>
+         </li>
+      </ul>
+   <form class="item-add" action="#" method="post">
+      <input type="text" name="name" placeholder="type a new item" autocomplete="off" required class="input">
+      <input type="submit" value="Add" class="submit">
+
+   </form>
+
+   </div>
+
+</BODY>
+</HTML>
